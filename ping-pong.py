33a6557888f8125c318305a0a257915aa286b88c
unknown <leonidlarge2019@gmail.com>
2026-01-20 18:29:35 +0500

    finish

--- ping-pong.py
+++ ping-pong.py
@@ -1,11 +1,17 @@
 from pygame import *
 
+font.init()
+
 window = display.set_mode((700, 500))
 display.set_caption('пинг-понг')
 background = transform.scale(image.load('fone.jpg'), (700, 500))
 
 clock = time.Clock()
 
+font1 = font.Font(None, 35)
+lose1 = font1.render('PLAYER_1 LOSE', True, (180, 0, 0))
+font2 = font.Font(None, 35)
+lose2 = font1.render('PLAYER_2 LOSE', True, (180, 0, 0))
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_width, player_height, speed):
@@ -36,10 +42,18 @@ class Player(GameSprite):
         if key_pressed[K_d] and self.rect.x < 600:
             self.rect.x += self.speed
 
-platforma1 = Player('platforma.jpg', 300, 20, 100, 20, 7)
-platforma2 = Player('platforma.jpg', 300, 470, 100, 20, 7)
 
 
+
+platforma1 = Player('platforma.jpg', 250, 20, 200, 20, 7)
+platforma2 = Player('platforma.jpg', 250, 470, 200, 20, 7)
+ball = GameSprite('ball.png', 350, 250, 40, 40, 3)
+
+speed_x = 3
+speed_y = 3
+
+
+finish = False
 game = True
 while game:
 
@@ -47,12 +61,33 @@ while game:
         if e.type == QUIT:
             game = False
 
-    platforma1.updated()
-    platforma2.updater()
 
-    window.blit(background, (0,0))
-    platforma1.draw()
-    platforma2.draw()
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        platforma1.updated()
+        platforma2.updater()
+        window.blit(background, (0,0))
+
+        if ball.rect.x > (700-50) or ball.rect.x < 0:
+            speed_x *= -1
+
+        if sprite.collide_rect(ball, platforma1) or sprite.collide_rect(ball, platforma2):
+            speed_y *= -1
+
+        if ball.rect.y == 0:
+            finish = True
+            window.blit(lose1, (250, 200))
+        
+        if ball.rect.y == 490:
+            finish = True
+            window.blit(lose2, (250, 200))
+
+
+        platforma1.draw()
+        platforma2.draw()
+        ball.draw()
 
 
 
